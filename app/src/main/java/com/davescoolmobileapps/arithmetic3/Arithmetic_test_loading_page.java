3590285ed6a429a64425df1c6ab68eae4dfbfbd4
FSCan <pstaight@gmail.com>
2017-04-02 16:01:14 -0700

    We added 3 textviews to the second screen, two for the numbers and one for the operation. again

--- app/src/main/java/com/davescoolmobileapps/arithmetic3/Arithmetic_test_loading_page.java
+++ app/src/main/java/com/davescoolmobileapps/arithmetic3/Arithmetic_test_loading_page.java
@@ -2,16 +2,12 @@ package com.davescoolmobileapps.arithmetic3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Editable;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
 
-public class MultiplicationTable extends AppCompatActivity {
+public class Arithmetic_test_loading_page extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_multiplication_table);
+        setContentView(R.layout.activity_arithmetic_test_loading_page);
     }
 }
