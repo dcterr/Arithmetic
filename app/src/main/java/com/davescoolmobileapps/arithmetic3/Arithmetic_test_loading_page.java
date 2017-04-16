a14ef584f8cf1220beeb061a4662fb53a3dd59c6
FSCan <pstaight@gmail.com>
2017-04-16 16:50:02 -0700

    Fixed some string literal warnings, and connected the Go button with the math test view.

--- app/src/main/java/com/davescoolmobileapps/arithmetic3/Arithmetic_test_loading_page.java
+++ app/src/main/java/com/davescoolmobileapps/arithmetic3/Arithmetic_test_loading_page.java
@@ -1,7 +1,13 @@
 package com.davescoolmobileapps.arithmetic3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+
+import static com.davescoolmobileapps.arithmetic3.R.id.difficulty_spinner;
+import static com.davescoolmobileapps.arithmetic3.R.id.problem_type_spinner;
 
 public class Arithmetic_test_loading_page extends AppCompatActivity {
 
@@ -10,4 +16,14 @@ public class Arithmetic_test_loading_page extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_arithmetic_test_loading_page);
     }
+
+    public void launchMathQuiz(View view) {
+        Intent intent = new Intent(this, ArithmeticTest.class);
+        Spinner ds = (Spinner) findViewById(difficulty_spinner);
+        Spinner pts = (Spinner) findViewById(problem_type_spinner);
+        intent.putExtra("LEVEL", ds.getSelectedItem().toString());
+        intent.putExtra("PROB_TYPE", pts.getSelectedItem().toString());
+        startActivity(intent);
+    }
+
 }
