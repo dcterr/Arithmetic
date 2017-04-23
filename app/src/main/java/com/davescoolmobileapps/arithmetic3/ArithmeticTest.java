2321bd8344e5b29744ba40717bd4b1f88826df83
FSCan <pstaight@gmail.com>
2017-04-23 16:59:01 -0700

    Read the intent extra variables. Implemented Addition case. fixed the size of student output.

--- app/src/main/java/com/davescoolmobileapps/arithmetic3/ArithmeticTest.java
+++ app/src/main/java/com/davescoolmobileapps/arithmetic3/ArithmeticTest.java
@@ -2,6 +2,15 @@ package com.davescoolmobileapps.arithmetic3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Random;
+
+import static com.davescoolmobileapps.arithmetic3.R.id.textView3;
+import static com.davescoolmobileapps.arithmetic3.R.id.textView4;
+import static com.davescoolmobileapps.arithmetic3.R.id.textView5;
+import static java.lang.Integer.parseInt;
+import static java.lang.Math.pow;
 
 public class ArithmeticTest extends AppCompatActivity {
 
@@ -9,5 +18,28 @@ public class ArithmeticTest extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_arithmetic_test);
+        Bundle extras = getIntent().getExtras();
+        int level = parseInt(extras.getString("LEVEL"));
+        String probType = extras.getString("PROB_TYPE");
+        TextView tv5 = (TextView) findViewById(textView5);
+        Random rnd = new Random();
+        int a=0,b=0,result;
+        if (probType.compareTo("Addition") == 0)
+        {
+            tv5.setText("+");
+            int min = level==1?0:(int) pow(10,level-1);
+            int max = (int) pow(10,level) -1;
+            a = min + rnd.nextInt(max-min);
+            b = min + rnd.nextInt(max-min);
+            result=a+b;
+        }
+        else if (probType.compareTo("Subtraction") == 0)
+        {
+            tv5.setText("-");
+        }
+        TextView tv3 = (TextView) findViewById(textView3);
+        tv3.setText(""+a);
+        TextView tv4 = (TextView) findViewById(textView4);
+        tv4.setText(""+b);
     }
 }
